1e03326e62837da1eb9753cd7bcddf6728c4ef23
csenguttuvan <chris.senguttuvan@kaltura.com>
2021-01-08 14:44:47 +0000

    opens a 3rd tab to github and credentials

--- web-automation.py
+++ web-automation.py
@@ -1,34 +1,45 @@
-from selenium import webdriver
 #Imports webdriver from Selenium
+from selenium import webdriver
+
 from  selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
-
-
-
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 # Imports time module
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--incognito")
+import time
 
-driver = webdriver.Chrome(chrome_options=chrome_options)
+
+chrome_options = webdriver.ChromeOptions()
+#Opens chrome in incognito mode
+chrome_options.add_argument("--incognito")
 #Using webdriver for chrome
+driver = webdriver.Chrome(chrome_options=chrome_options)
+#Gets Kaltura.okta.com HTTP page
 driver.get('https://kaltura.okta.com')
-#Will open kaltura.okta.com on chrome
+
+#assign variables to username and password field of okta. 
 username = driver.find_element_by_xpath('//*[@id="okta-signin-username"]')
 password = driver.find_element_by_xpath('//*[@id="okta-signin-password"]')
-#assign variables to username and password field of okta. 
-username.send_keys('username')
-password.send_keys('password')
-#sends the username and password to the fields
+
+#sends the username and password to the respective fields on the web page
+username.send_keys('ADM.ChrisS@kaltura.com')
+password.send_keys('pass')
+#checks the 'remember me' check box and clicks 'Sign In'
 rememberMe = driver.find_element_by_xpath('//*[@id="form1"]/div[1]/div[2]/div[3]/div/span/div/label').click()
 signinButton = driver.find_element_by_xpath('//*[@id="okta-signin-submit"]').click()
-#checks the remember me check box and signs in
-time.sleep(6)
-#waits for 5 sec
+# Waits for page to fully load to proceed
+delay = 3
+try:
+    meElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
+    print ("Page is ready")
+except TimeoutException:
+    print ("Loading took too much time!")
 
 sendPushButton = driver.find_element_by_xpath('//*[@id="form8"]/div[2]/input').click()
 sendPushCheckBox = driver.find_element_by_xpath('//*[@id="okta-sign-in"]/div[2]/div/div/span/div/label').click()
-
+#opens a second tab on chrome
 driver.execute_script("window.open('about:blank', 'tab2');")
 driver.switch_to_window("tab2")
 driver.get('https://admin.microsoft.com/Adminportal')
@@ -36,8 +47,30 @@ time.sleep(1)
 microsoftEmail = driver.find_element_by_xpath('//*[@id="i0116"]')
 microsoftEmail.send_keys('ADM.ChrisS@kaltura.com')
 microsoftNext = driver.find_element_by_xpath('//*[@id="idSIButton9"]').click()
-time.sleep(2)
-username2 = driver.find_element_by_xpath('//*[@id="form1"]/div[1]/div[2]/div[1]/div[2]/span').clear
-username2.send_keys('ADM.ChrisS@kaltura.com')
-password.send_keys('Goldsmith1992Q')
+delay = 3
+try:
+    meElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
+    print ("Page is ready")
+except TimeoutException:
+    print ("Loading took too much time!")
+username = driver.find_element_by_xpath('//*[@id="okta-signin-username"]')
+username.clear()
+username.send_keys('ADM.ChrisS@kaltura.com')
+password = driver.find_element_by_xpath('//*[@id="okta-signin-password"]')
+password.send_keys('pass')
 signinButton = driver.find_element_by_xpath('//*[@id="okta-signin-submit"]').click()
+driver.execute_script("window.open('about:blank', 'tab3');")
+driver.switch_to_window("tab3")
+driver.get('https://github.com')
+signInGithub = driver.find_element_by_xpath('/html/body/div[1]/header/div/div[2]/div[2]/a[1]').click()
+delay = 3
+try:
+    meElem = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'IdOfMyElement')))
+    print ("Page is ready")
+except TimeoutException:
+    print ("Loading took too much time!")
+githubUsername = driver.find_element_by_xpath('//*[@id="login_field"]')
+githubUsername.send_keys('corporate-it@kaltura.com')
+githubPassword = driver.find_element_by_xpath('//*[@id="password"]')
+githubPassword.send_keys('pass')
+githubSignIn = driver.find_element_by_xpath('//*[@id="login"]/div[4]/form/input[14]').click()
